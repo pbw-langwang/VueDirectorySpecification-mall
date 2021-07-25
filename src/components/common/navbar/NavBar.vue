ead15c6234eb3a92eef72d02aa851607ad09742f
未来 <1911847149@qq.com>
2021-07-25 22:37:23 +0800

    2021/7/25

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="nav-bar">
+    <div class="left">
+      <slot name="left">左</slot>
+    </div>
+    <div class="center">
+      <slot name="center">中</slot>
+    </div>
+    <div class="right">
+      <slot name="right">右</slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"NavBar",
+}
+</script>
+
+<style scoped>
+  .nav-bar{
+    display: flex;
+    /* 没有内容,所以只设置line-height无效 */
+    height: 44px;
+    line-height: 44px;
+    text-align: center;
+    box-shadow: 0px 1px 2px rgba(0, 0, 0, .2);
+  }
+  
+  .left,.right{
+    width: 15vw;
+    /* background-color: aqua; */
+  }
+
+  .center{
+    flex: 1;
+    /* background-color: blue; */
+  }
+</style>
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,15 +1,32 @@
 <template>
-  <div>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="left"></div>
+      <div slot="center">
+        <p>购物街</p>
+      </div>
+      <div slot="right"></div>
+    </nav-bar>
     <p style="font-size:21px">首页</p>
     <p style="font-size:2rem">首页</p>
   </div>
 </template>
 
 <script>
+  import NavBar from "components/common/navbar/NavBar.vue"
+
   export default {
-    name:"",
+    name:"Home",
     components:{
-      
+      NavBar
     }
   }
 </script>
+
+<style scoped>
+  .home-nav{
+    background-color: var(--color-tint);
+    font-size: 1.5rem;
+    color: white;
+  }
+</style>
