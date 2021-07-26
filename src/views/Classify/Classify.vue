f772edc032b809083acec51841216229d9de62ba
未来 <1911847149@qq.com>
2021-07-26 17:11:11 +0800

    封装了网络请求和头部导航

--- src/views/Classify/Classify.vue
+++ src/views/Classify/Classify.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
+    <classify-nav-bar/>
     <p>分类</p>
   </div>
 </template>
 
 <script>
+  import ClassifyNavBar from "views/Classify/components/ClassifyNavBar.vue"
   export default {
-
+    components:{
+      ClassifyNavBar
+    }
   }
 </script>
 
---
+++ src/views/Classify/components/ClassifyNavBar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <nav-bar>
+      <div slot="left">
+        <img class="back" src="~assets/img/common/back.svg">
+      </div>
+      <div slot="center" class="flex-row-spaceAround">
+        <div v-for="(item,index) in titleList" :key="item"  class="flex1" 
+          :class="index == currenctIndex ? 'active':''"
+          @click="getIndex(index)"
+        >
+          {{item}}
+        </div>
+      </div>
+      <div slot="right"></div>
+    </nav-bar>
+  </div>
+</template>
+
+<script>
+  import NavBar from "components/common/navbar/NavBar.vue"
+
+  export default {
+    components:{
+      NavBar
+    },
+    data(){
+      return {
+        titleList:["参数","详情","评论","推荐"],
+        currenctIndex:0,
+      }
+    },
+    methods:{
+      getIndex(index){
+        this.currenctIndex = index;
+      },
+    }
+  }
+</script>
+
+<style scoped>
+  .active{
+    color: var(--color-high-text);
+  }
+  .back{
+    width: 40%;
+  }
+</style>
