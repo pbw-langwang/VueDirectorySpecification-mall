b8456be0ab7f7811dde949857d5bae8caa2eb838
未来 <1911847149@qq.com>
2021-07-28 16:56:33 +0800

    封装了轮播图

---
+++ src/views/Home/components/HmoeSwiper.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <swiper>
+      <swiper-item v-for="(item,index) in banner" :key="index">
+        <a :href="item.link">
+          <img :src="item.image">
+        </a>
+      </swiper-item>
+    </swiper>
+  </div>
+</template>
+
+<script>
+  import {Swiper,SwiperItem} from 'components/common/swiper/index.js'
+  export default {
+    name:"HomeSwiper",
+    components:{
+      Swiper,
+      SwiperItem
+    },
+    props:{
+      banner:{
+        type:Array,
+        default(){
+          return []
+        }
+      }
+    }
+  }
+</script>
